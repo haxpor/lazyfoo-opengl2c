fa940d8a6ce57eee82159d17a539017952b162ab
Wasin Thonkaew <haxpor@gmail.com>
2018-12-26 05:55:24 +0800

    Add 13

---
+++ 13_matrixTransformations/gl/gl_types.h
@@ -0,0 +1,20 @@
+#ifndef gl_types_h_
+#define gl_types_h_
+
+#include "gl.h"
+
+typedef struct
+{
+  GLfloat x;
+  GLfloat y;
+  GLfloat w;
+  GLfloat h;
+} LFRect;
+
+typedef struct
+{
+  GLfloat w;
+  GLfloat h;
+} LFSize;
+
+#endif
