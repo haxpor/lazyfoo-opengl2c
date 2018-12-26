936b12638cc45d19696cef790e0303b833b84703
Wasin Thonkaew <haxpor@gmail.com>
2018-12-26 11:37:23 +0800

    Add 16

---
+++ 16_vertexArrays/README.md
@@ -0,0 +1,3 @@
+# Changes from original
+
+* Clear background to cyan as to be able to differentiate quad whether it's drew. Due to all of our code clears background with white color.
---
+++ 16_vertexArrays/gl/gl_types.h
@@ -0,0 +1,26 @@
+#ifndef gl_types_h_
+#define gl_types_h_
+
+#include "gl.h"
+
+typedef struct
+{
+  GLfloat x;
+  GLfloat y;
+  GLfloat w;
+  GLfloat h;
+} LFRect;
+
+typedef struct
+{
+  GLfloat w;
+  GLfloat h;
+} LFSize;
+
+typedef struct
+{
+  GLfloat x;
+  GLfloat y;
+} LFVector2D;
+
+#endif
